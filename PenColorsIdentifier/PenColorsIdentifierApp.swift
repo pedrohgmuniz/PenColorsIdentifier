679f67b777e06cc8bd699b0e1d54f5ae6a696a18
Pedro Muniz <phgmuniz@gmail.com>
2023-05-04 10:16:15 -0300

    First Commit. Project created

---
+++ PenColorsIdentifier/PenColorsIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenColorsIdentifierApp.swift
+//  PenColorsIdentifier
+//
+//  Created by Pedro Muniz on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PenColorsIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
